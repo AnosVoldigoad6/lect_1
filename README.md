e1614fb8ec1f9aab7679388fecc77f3b48e63cfa
Sap Point <emptied1nside@gmail.com>
2023-11-14 15:04:59 +0500

    lecct-1

---
+++ README.md
@@ -0,0 +1,3 @@
+# What is JS?
+## bgrbtrgriu
+> ghtrghtoightlrghiohir
---
+++ app.js
@@ -0,0 +1,9 @@
+let str = ' ';
+let cnt = 0
+for (let i = 0; i<9; i++){
+    str+=i;
+    cnt+=i
+
+}
+console.log(str);
+console.log(cnt);
